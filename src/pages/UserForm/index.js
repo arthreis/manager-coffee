f2ecc622bd065ef29c0241f7e9acd77e50bc58d8
Arthur <arr90rj@gmail.com>
2019-05-29 01:00:04 -0300

    add field validation

--- src/pages/UserForm/index.js
+++ src/pages/UserForm/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { withFormik } from 'formik';
+import * as Yup from 'yup';
 
 import { coffeeService } from '../../services/coffeeService';
 
-const UserForm = ({ handleSubmit, values, handleChange, setFieldValue }) => (
+const UserForm = ({ handleSubmit, errors, values, handleChange, setFieldValue }) => (
     <form onSubmit={handleSubmit}>
         <input type="text"     placeholder="Name"        name="name"        onChange={handleChange} value={values.name}/>
+        <br/>{ !!errors.name && <span>{errors.name}</span> }
         <input type="textarea" placeholder="Description" name="description" onChange={handleChange} value={values.description}/>
+        <br/>{ !!errors.description && <span>{errors.description}</span> }
         <input type="number"   placeholder="Price"       name="price"       onChange={ event => setFieldValue('price', event.target.value)} value={values.price}/>
-
+        <br/>{ !!errors.price && <span>{errors.price}</span> }
+        <br/>
         <button type="submit">Send</button>
     </form>
 );
@@ -19,6 +23,17 @@ export default withFormik({
         description: '',
         price: ''
     }),
+
+    validateOnChange: false,
+
+    validateOnBlur: false,
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Required field'),
+        description: Yup.string(),
+        price: Yup.number().positive("Can't be negative").required('Required field'),
+    }),
+
     handleSubmit: (values, { props }) => {
         const { id } = props.match.params;
         if (id) {
